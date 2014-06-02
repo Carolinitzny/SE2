26a9cfab5fdf314914288b24e45e94ccb5cfadea
Kitzky <Yannic.kitzky@gmx.net>
2014-06-02 15:40:54 +0200

    version der letzten SE2 Stunde

---
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/Beobachtbar.java
@@ -0,0 +1,28 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public abstract class Beobachtbar
+{
+    Set<Beobachter> _beobachter;
+    
+    public Beobachtbar()
+    {
+        _beobachter = new HashSet<Beobachter>();
+        
+    }
+    
+    public void setzeBeobachtbae(Beobachter beobachter)
+    {
+        _beobachter.add(beobachter);
+    }
+    
+    public void meldeAenderung(String id)
+    {
+       for(Beobachter b : _beobachter)
+       {
+           b.reagiereAufAenderung(id);
+       }
+    }
+}
---
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/Beobachter.java
@@ -0,0 +1,8 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
+
+public interface Beobachter
+{
+
+void reagiereAufAenderung(String _id);    
+    
+}
