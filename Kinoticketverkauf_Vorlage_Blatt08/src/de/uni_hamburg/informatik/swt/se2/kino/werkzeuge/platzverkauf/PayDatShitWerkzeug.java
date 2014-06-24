2ac345f4c032c484b8078bdfadc584616d23f771
Yannic Kitzky <3kitzky@informatik.uni-hamburg.de>
2014-06-24 16:15:23 +0200

    Kommentare und Vorbedingungen im PayDatShitWerzeug hinzugefügt

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/platzverkauf/PayDatShitWerkzeug.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/platzverkauf/PayDatShitWerkzeug.java
@@ -9,6 +9,15 @@ import javax.swing.JPanel;
 
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Geld;
 
+/**
+ * Werkzeug für den Bezahlvorgang, gibt zu bezahlenden Betrag und Restbetrag an.
+ * 
+ * 
+ * 
+ * @author 
+ *
+ */
+
 public class PayDatShitWerkzeug 
 {
 	public boolean _istBezahlt = false;
@@ -16,6 +25,15 @@ public class PayDatShitWerkzeug
 	private int _preis;
 	private PayDatShitWerkzeugUI _ui;
 	private int _bargeld;
+	
+    /**
+     * Erstellt ein neues Werzeug
+     * 
+     * @param preis Preis der insgesamt bezahlt werden soll, in Eurocent
+     * 
+     * 
+     */
+	
 	public PayDatShitWerkzeug(int preis)
 	{
 		_preis = preis;
@@ -24,23 +42,54 @@ public class PayDatShitWerkzeug
 		_ui.getDialog().setVisible(true);
 		
 	}
+	
+	/**
+	 * Gibt den zu zahlenden Preis zurück
+	 * 
+	 * @return zu zahlender Preis in Eurocent
+	 */
 	public int gibPreis()
 	{
 		return _preis;
 		
 	}
+	
+	/**
+	 * Berechnet die Differenz zwischen dem zu zahlenden Betrag und dem gezahlten Betrag
+	 * 
+	 * @param preis der zu zahlende Preis
+	 * @param bargeld der schon gezahlte Preis
+	 * @return Restbetrag
+	 * 
+	 * @require bargeld darf kein negativer Betrag sein
+	 */
 	public int berechneRestbetrag(int preis, int bargeld)
 	{
+	    assert(bargeld>=0):"Bargeld darf kein negativer Wert sein";
 		int restbetrag = preis-bargeld;
 		_ui.getRestbetragLabel().setText(String.valueOf(restbetrag*-1)+" Eurocent");
 		return preis-bargeld;
 	}
 	
+	/**
+	 * Prüft, ob der Preis bezahlt wurde
+	 * 
+	 * @param bargeld der schon gezahlte Preis
+	 * @return Gibt zurück, ob genug bezahlt wurde
+	 * 
+     * @require bargeld darf kein negativer Betrag sein
+	 */
 	public boolean istBezahlt(int bargeld)
 	{
+	    assert(bargeld>=0):"Bargeld darf kein negativer Wert sein";
 		_istBezahlt = berechneRestbetrag(_preis, bargeld) < 0;
 		return berechneRestbetrag(_preis, bargeld) < 0;
 	}
+	
+	/**
+	 * Interaktion mit dem Userinterface, bei Betätigung des Abbrechen-Buttons in der UI wird der Dialog geschlossen
+	 * und somit das restliche Programm fortgesetzt
+	 */
 	public void registriereUIaktionen()
 	{
 		
